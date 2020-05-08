7c2c920afc5a60fa4c122b4e105b01399f9e55d3
Shahid <shahidaf830@gmail.com>
2020-05-08 11:35:03 +0530

    initial commit

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const app=express();
 const bodyParser=require("body-parser");
 app.use(bodyParser.urlencoded({extended:true}));
 app.set("view engine","ejs");
+const PORT=process.env.PORT || 3000;
 app.use(express.static(__dirname+"/public"));
 app.get('/',(req,res)=>{
     res.render('home')
@@ -19,7 +20,7 @@ app.get('/contact',(req,res)=>{
 app.get('/project',(req,res)=>{
     res.render('project');
 })
-app.listen(3000,(err)=>{
+app.listen(PORT,(err)=>{
     if(err){
         console.log(err);
     }else{
